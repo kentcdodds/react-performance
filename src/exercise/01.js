a7d29bf20e3b74a47138662aa1d298d4f835f951
Aurel Ioan Patrutiu <aurel.patrutiu@sage.com>
2026-07-22 14:58:47 +0200

    ex-1-extra

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,6 +5,10 @@ import * as React from 'react'
 
 const Globe = React.lazy(() => import('../globe'));
 
+function loadGlobe() {
+  return import('../globe')
+}
+
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
 
@@ -19,7 +23,7 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label style={{marginBottom: '1rem'}} onMouseEnter={loadGlobe} onMouseFocus={loadGlobe}>
         <input
           type="checkbox"
           checked={showGlobe}
