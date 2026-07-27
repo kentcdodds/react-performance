955e6f190b2c508f2ebe63a49c3e39f754656c56
Aurel Ioan Patrutiu <aurel.patrutiu@sage.com>
2026-07-27 09:13:33 +0200

    ex-2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -59,9 +59,7 @@ function ListItem({
 function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
-
-  // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = getItems(inputValue)
+  const allItems = React.useMemo(() => getItems(inputValue), [inputValue] ) 
   const items = allItems.slice(0, 100)
 
   const {
